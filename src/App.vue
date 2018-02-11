7be7037618144447bec27e53228defac32d402e3
Sangeeth_ks <sangeeth.ks@mobiotics.com>
2018-02-11 09:27:41 +0530

    SCSS started

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
-    <header>
-      <span>iamsangeeth</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
-    </main>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -16,39 +10,10 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../static/scss/modules/_variables.scss';
+
 body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
+  background-color: $bg-color;
 }
 </style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+@import '../../../static/scss/modules/variables.scss';
+</style>
