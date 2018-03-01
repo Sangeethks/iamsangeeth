74de0e6a250e5dd1b9d1faa6c0f1bec94406b4bd
Sangeethks <sangeethks.in@gmail.com>
2018-03-01 12:56:40 +0530

    Projects page - Changed back to under construction mode

--- src/components/projects/Projects.vue
+++ src/components/projects/Projects.vue
@@ -2,14 +2,14 @@
   <div class="ims-container">
     <cmp-header></cmp-header>
 
-    <!-- <cmp-under-construction></cmp-under-construction> -->
+    <cmp-under-construction></cmp-under-construction>
 
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-md-6" v-for="i in 5" :key="i">
             <slot-portfolio-card></slot-portfolio-card>
         </div>
         <div class="clearfix"></div>
-    </div>
+    </div> -->
 
     <cmp-footer></cmp-footer>
   </div>
@@ -18,7 +18,7 @@
 
 <script>
 import Header from '@/components/shared/Header.vue'
-// import UnderConstruction from '@/components/shared/UnderConstruction.vue'
+import UnderConstruction from '@/components/shared/UnderConstruction.vue'
 
 const Footer = () =>
   import(/* webpackChunkName: "Footer" */ '@/components/shared/Footer.vue')
@@ -28,7 +28,7 @@ const PortfolioCard = () =>
 export default {
   components: {
     'cmp-header': Header,
-    // 'cmp-under-construction': UnderConstruction,
+    'cmp-under-construction': UnderConstruction,
     'slot-portfolio-card': PortfolioCard,
     'cmp-footer': Footer
   }
